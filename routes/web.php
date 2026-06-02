d72bdfa8aab767ff6f430ae443894bb3e33bbe4f
noorafifi889 <noorelafifi889@gmail.com>
2026-06-02 15:32:23 +0300

    fixed a  request and controller in auth function and ui

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TaskController;
-
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-
-Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])
-    ->name('tasks.edit');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', function () {
+    return view('index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    /* ---- استبدل راوت الـ tasks القديم بهذا السطر ---- */
+    Route::resource('tasks', TaskController::class);
+});
+require __DIR__.'/auth.php';
